3013f1b46fc7d46e853e9a6ae42c37f3e3412a98
Alexa <alexa.anderson@outlook.com>
2016-11-22 13:33:11 -0600

    Add rticle form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles, except: [:destroy, :edit, :update]
+  resources :articles, except: [:destroy, :update]
+  get '/new', to: 'articles#new'
 
   root to: 'articles#index'
 end
