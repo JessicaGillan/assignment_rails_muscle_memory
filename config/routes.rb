11dbfb53bc46f086ec8a0d691eada7001729db00
Alexa <alexa.anderson@outlook.com>
2016-11-22 11:59:18 -0600

    Add article pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/new', to: 'articles#new'
+  post '/new', to: 'articles#create'
+  get '/article/:id', to: 'articles#show'
 end
