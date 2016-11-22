c209157cebc2f7713b6479a791847239cc1b1b80
Jessica G <gillan.jessica@gmail.com>
2016-11-22 12:08:47 -0700

    get articles show and index live

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,11 +11,14 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
 
+  def index
+    @articles = Article.all
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/new', to: 'articles#new'
-  post '/new', to: 'articles#create'
-  get '/article/:id', to: 'articles#show'
+  resources :articles, except: [:destroy, :edit, :update]
+
   root to: 'articles#index'
 end
